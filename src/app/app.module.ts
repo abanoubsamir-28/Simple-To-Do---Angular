b904e862e9118ffe4326bcfc3f2f38aad799a825
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2021-10-12 05:10:53 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { InputComponent } from './input/input.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    InputComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
